256d9de722ffb191b4b498f597adc8db82a000b5
Hichem2h <hichem2h@gmail.com>
2018-03-01 17:44:41 +0100

    edit: improve form validations

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Regexp, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(message='Insert your username')])
-    pin = PasswordField('Pin Code', validators=[DataRequired(message='Insert your pin code'), Length(min = 6, max = 6, message='Pin must be 6 digits long')])
+    password = PasswordField('Password', validators=[DataRequired(message='Insert your password')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(message='Insert a username')])
-    birth_date = DateField('Date Of Birth', validators=[DataRequired(message='Enter a valid Date (dd/mm/YYYY)')], format='%d/%m/%Y',\
+    username = StringField('Username', validators=[
+        DataRequired(message='Insert a username'),
+        Regexp('\w', message='Username must contain only letters and digits')])
+
+    birth_date = DateField('Date Of Birth',
+                           validators=[DataRequired(message='Enter a valid Date (dd/mm/YYYY)')],
+                           format='%d/%m/%Y',\
                            render_kw={'placeholder': '20/06/1996 for June 20, 1996'})
-    pin = PasswordField('Pin Code', validators=[DataRequired(message='Insert a pin code'), Length(min = 6, max = 6, message='Pin must be 6 digits long')])
-    pin_repeat = PasswordField('Repeat Pin Code', validators=[DataRequired(message='Confirm pin code'), EqualTo('pin', message='Pin codes don\'t match')])
+
+    password = PasswordField('Password', validators=[
+        DataRequired(message='Insert a password'),
+        Length(min = 6, message='Password must be at least 6 characters')])
+
+    password2 = PasswordField('Repeat Password',
+                              validators=[DataRequired(message='Confirm password'),
+                                          EqualTo('pin', message='Passwords don\'t match')])
+
     submit = SubmitField('Make Registration')
