0b829cf025f8d3fdd4760018c292992bc4beaa75
Hichem2h <hichem2h@gmail.com>
2018-03-05 21:48:44 +0100

    fix: database bug

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'developement key'
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'developement key'
+    DEBUG = os.environ.get('DEBUG') or True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:////' + basedir + '/app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class OtherConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@localhost/mac'
